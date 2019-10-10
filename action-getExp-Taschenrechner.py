3f7025ee1925eaa5ee4a20939cb23a50703f3c8a
Luis <23378442+daandtu@users.noreply.github.com>
2019-10-10 11:28:47 +0200

    Added exp action

--- action-getExp-Taschenrechner.py
+++ action-getExp-Taschenrechner.py
@@ -1,1 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+from hermes_python.hermes import Hermes
+
+
+def action_wrapper(hermes, intent_message):
+    first = int(intent_message.slots.firstTerm.first().value)
+    second = int(intent_message.slots.secondTerm.first().value)
+    
+    calc = first ** second
+    
+    if str(calc)[-2:] == ".0":
+        calc = int(calc)
+    if "." in str(calc):
+        calc_part1, calc_part2 = str(calc).split(".")
+        result_sentence = "{0} hoch {1} ergibt {2} komma {3} .".format(first, second, calc_part1, calc_part2)
+    else:
+        result_sentence = "{} hoch {} ergibt {} .".format(first, second, calc)
+    
+    current_session_id = intent_message.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)
+
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent("cluisc:getExp", action_wrapper).start()
